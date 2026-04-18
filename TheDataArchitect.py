4635043d62ec012ef052211985ed902725ba9084
Jubayer Rahman <rjubayer329@gmail.com>
2026-04-18 19:06:01 +0800

    Added JSON persistence and error handling

--- TheDataArchitect.py
+++ TheDataArchitect.py
@@ -1,3 +1,4 @@
+import json
 library = {
     "AI":["Superintelligence", "life 3.0", "How an AI can be a teacher like Gimini"],
     "Python":["Automate the boring stuff"],
@@ -21,7 +22,23 @@ def show_book():
         for title in library[catagory]:
             print(f"{catagory} -> {title}")
     print("------------------------------------")
+
+def save_data():
+    with open("libraryData.json", "w") as file:
+        json.dump(library, file)
+        print("Data seved to the voult✅")
+
+def load_data():
+    global library
+    try:
+        with open("libraryData.json", "r") as file:
+            library = json.load(file)
+            print("The file has loaded✅")
+    except FileNotFoundError:
+        print("We couldn't find the file!")
+load_data()
 input1 = input("What's your book catagory?: ").strip().capitalize()
 input2 = input("What's the title of the book?: ").strip().capitalize()
 add_book(input1, input2)
 show_book()
+save_data()
