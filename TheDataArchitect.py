91ef13eb249f0eaedfa7637dcb3a9687a2555503
Jubayer Rahman <rjubayer329@gmail.com>
2026-04-19 22:27:37 +0800

    Integrated Smart Classifier with Human-in-the-loop logic

--- TheDataArchitect.py
+++ TheDataArchitect.py
@@ -4,6 +4,12 @@ library = {
     "Python":["Automate the boring stuff"],
     "History": ["The history of Bangladesh"]
 }
+brain = {
+    "Python": ["Python", "Script", "Code", "Automate"],
+    "Ai": ["Neural", "Intelligence", "Data", "Robot", "Machine"],
+    "History": ["History", "Ancient", "War", "Century"]
+}
+brain = {k.lower(): [w.lower() for w in v] for k, v in brain.items()}
 library = {k.capitalize(): v for k, v in library.items()}
 def add_book(catagory, title):
     if catagory in library:
@@ -17,11 +23,12 @@ def add_book(catagory, title):
         print("A new book is added")
     
 def show_book():
-    print("------------------------------------")
-    for catagory in library:
-        for title in library[catagory]:
-            print(f"{catagory} -> {title}")
-    print("------------------------------------")
+    print("\n" + "-"*30)
+    for catagory, books in library.items():
+        print(f"[{catagory.upper()}]")
+        for title in books:
+            print(f" > {title}")
+    print("-"*30 + "\n")
 
 def save_data():
     with open("libraryData.json", "w") as file:
@@ -36,9 +43,29 @@ def load_data():
             print("The file has loaded✅")
     except FileNotFoundError:
         print("We couldn't find the file!")
+
+def guess_catagory(title):
+    title_clean = title.strip().lower()
+    
+    for catagory, keywords in brain.items():
+        for word in keywords:
+            if word in title_clean:
+                return catagory
+    return "Unknown"
+
 load_data()
-input1 = input("What's your book catagory?: ").strip().capitalize()
-input2 = input("What's the title of the book?: ").strip().capitalize()
-add_book(input1, input2)
+user_title = input("Enter the title of the book: ")
+prediction = guess_catagory(user_title)
+if prediction != "Unknown":
+    print(f"🤖 Your book catagory might be {prediction}")
+    confermation = input("Do you want to conferm this catagory?: ").lower()
+    if confermation == "yes":
+        final_cat = prediction
+    else:
+        final_cat = input("So, please enter the catagory: ")
+else:
+    print("🤖 I'm not sure about the catagory of this book!")
+    final_cat = input("Please enter the catagory of this book: ")
+add_book(final_cat, user_title)
 show_book()
 save_data()
